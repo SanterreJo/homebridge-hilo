12892b405f39a0c68255633a196e755929ef676f
Jonathan Santerre <jonathan@santerre.dev>
2024-04-11 19:42:53 -0400

    send codeverifier to token request

--- homebridge-ui/server.js
+++ homebridge-ui/server.js
@@ -6,9 +6,16 @@ const port = 8880;
 class UiServer extends HomebridgePluginUiServer {
 	constructor() {
 		super();
-		const app = express();
-		app.get("/auth/external/callback", async (req, res) => {
-			const state = req.query.state;
+		this.app = express();
+		this.app.listen(port, () => {
+			console.log(`Auth server listening on port ${port}`);
+		});
+		this.onRequest("/callback", this.handleCallback.bind(this));
+		this.this.ready();
+	}
+
+	async handleCallback({ codeVerifier }) {
+		this.app.get("/auth/external/callback", async (req, res) => {
 			const code = req.query.code;
 			const tokenResponse = await fetch(
 				"https://connexion.hiloenergie.com/hilodirectoryb2c.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1a_sign_in",
@@ -20,22 +27,15 @@ class UiServer extends HomebridgePluginUiServer {
 					body: new URLSearchParams({
 						client_id: "1ca9f585-4a55-4085-8e30-9746a65fa561",
 						code,
+						code_verifier: codeVerifier,
 						grant_type: "authorization_code",
-						state,
 						redirect_uri: "https://my.home-assistant.io/redirect/oauth",
-						scope:
-							"openid https://HiloDirectoryB2C.onmicrosoft.com/hiloapis/user_impersonation offline_access",
 					}),
 				}
 			);
 			console.log(tokenResponse);
-			res.sendStatus(200);
+			res.send("Success - you can now close this window");
 		});
-
-		app.listen(port, () => {
-			console.log(`Auth server listening on port ${port}`);
-		});
-		this.ready();
 	}
 }
 
