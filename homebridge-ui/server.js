4967aa882689755c538dee23ccd6aa879a3b7a16
Jonathan Santerre <jonathan@santerre.dev>
2024-04-11 19:50:41 -0400

    wait for token response

--- homebridge-ui/server.js
+++ homebridge-ui/server.js
@@ -15,28 +15,39 @@ class UiServer extends HomebridgePluginUiServer {
 	}
 
 	async handleCallback({ codeVerifier }) {
-		this.app.get("/auth/external/callback", async (req, res) => {
-			const code = req.query.code;
-			const tokenResponse = await fetch(
-				"https://connexion.hiloenergie.com/hilodirectoryb2c.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1a_sign_in",
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/x-www-form-urlencoded",
-					},
-					body: new URLSearchParams({
-						client_id: "1ca9f585-4a55-4085-8e30-9746a65fa561",
-						code,
-						code_verifier: codeVerifier,
-						grant_type: "authorization_code",
-						redirect_uri: "https://my.home-assistant.io/redirect/oauth",
-					}),
-				}
-			);
-			const body = await tokenResponse.json();
-			console.log(body);
-			const accessToken = body.access_token;
-			res.send("Success - you can now close this window");
+		return new Promise((resolve) => {
+			this.app.get("/auth/external/callback", async (req, res) => {
+				const code = req.query.code;
+				const tokenResponse = await fetch(
+					"https://connexion.hiloenergie.com/hilodirectoryb2c.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1a_sign_in",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/x-www-form-urlencoded",
+						},
+						body: new URLSearchParams({
+							client_id: "1ca9f585-4a55-4085-8e30-9746a65fa561",
+							code,
+							code_verifier: codeVerifier,
+							grant_type: "authorization_code",
+							redirect_uri: "https://my.home-assistant.io/redirect/oauth",
+						}),
+					}
+				);
+				const body = await tokenResponse.json();
+				console.log(body);
+				const accessToken = body.access_token;
+				const refreshToken = body.refresh_token;
+				const expiresIn = body.expires_in;
+				const refreshTokenExpiresIn = body.refresh_token_expires_in;
+				res.send("Success - you can now close this window");
+				resolve({
+					accessToken,
+					refreshToken,
+					expiresIn,
+					refreshTokenExpiresIn,
+				});
+			});
 		});
 	}
 }
