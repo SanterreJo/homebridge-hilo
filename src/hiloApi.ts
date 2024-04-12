f9fdad58cb00682b7777ba9f7552975b252d4df2
Jonathan Santerre <jonathan@santerre.dev>
2024-04-12 11:33:07 -0400

    fix renewtokens

--- src/hiloApi.ts
+++ src/hiloApi.ts
@@ -39,7 +39,7 @@ async function login() {
 	getLogger().debug("Logging in");
 	const config = getConfig();
 	const newRefreshToken = config.refreshToken;
-	renewTokens({
+	await renewTokens({
 		newRefreshToken,
 	});
 }
